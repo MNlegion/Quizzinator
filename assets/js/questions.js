d2b9290daff8dd72011ea84f3d7ed0ca5ad9153b
MNlegion <rkreuser30@gmail.com>
2022-05-19 22:34:21 -0500

    questions.js file added, route added to index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      question: "Learning to code is challenging?",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      question: "Visual Studio Code is a programming language?",
+      choices: ["True", "False"],
+      answer: "False"
+    },
+    {
+      question: "Which CSS selector should only be applied to one element on the page?",
+      choices: ["div", "id", "tag", "var"],
+      answer: "id"
+  
+    },
+    {
+      question: "NPM packages are already stored within VS Code for your use.",
+      choices: ["true - all packages are available", "false - packages must be installed from the npm site"],
+      answer: "false - packages must be installed from the npm site"
+    },
+    {
+      question: "What does this refer to?",
+      choices: ["this computer", "this variable", "the parent object of the target"],
+      answer: "the parent object of the target"
+    }
+  ]
