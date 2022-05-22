48743ba800fb8e5a6c83a06cd27ae7496edf9b90
MNlegion <rkreuser30@gmail.com>
2022-05-22 16:43:53 -0500

    reworked script.js to seperate questions into questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,191 +1,267 @@
-var s=59
-var m=1
-var q
+/* create variables to refrence all DOM elements: */
+//start game button
+var startGameBtnEl = document.querySelector("#start-game-btn");
+//start-screen element
+var startScreenEl = document.querySelector("#start-screen");
+
+//quiz-content (event delegation)
+var quizScreenEl = document.querySelector("#quiz-content");
+
+var headerEl = document.querySelector("header");
+//time-left
+var timeLeftEl = document.querySelector("#time-left");
+//view-highscores
+var viewHighscoresAEl = document.querySelector("#view-highscores");
+
+//post-game-screen
+var postGameScreenEl = document.querySelector("#post-game-screen");
+//user-score
+var userScoreEl = document.querySelector("#user-score");
+//play-again-btn
+
+var playAgainBtnEl = document.querySelector("#play-again-btn");
+//add-highscore-btn
+var addHighscoreBtnEl = document.querySelector("#add-highscore-btn");
+
+//var highScoresListEl
+var highScoresListEl = document.querySelector("#high-scores-list");
+
+//answer-info
+var answerInfoEl = document.querySelector("#answer-info");
+//answer-info-text
+var answerInfoTextEl = document.querySelector("#answer-info-text");
+
+//highscore div
+var gameEndFormEl = document.querySelector("#game-end-form");
+
+var userInitials = document.querySelector("#user-initials");
+
+var clearHighScoresBtnEl = document.querySelector("#clear-highscore");
+
+/* create variables for game logic: */
+//timerIntervalId - set timer to variable so we can stop it
+var timerIntervalId;
+//score
+var score;
+//secondsLeft
+var secondsLeft;
+
+var highScoresDisplayEl = document.querySelector("#high-scores-display");
+
+var highScoresList = [];
+if (localStorage.getItem("highScoresList")){
+  highScoresList = JSON.parse(localStorage.getItem("highScoresList"));
+}
+
+// create function to start game
+function startGameHandler(event) {
+  event.preventDefault();
+  // set secondsLeft variable to starting time (300 seconds = 5 minutes)
+  secondsLeft = (questions.length) * 15;
+  // write secondsLeft to the page
+  timeLeftEl.textContent = secondsLeft;
+
+  // reset score to 0
+  score = 0;
+
+  // hide start-screen element && post-game-screen
+  headerEl.classList.remove("invisible");
+  startScreenEl.classList.add("hide");
+  postGameScreenEl.classList.add("hide");
+  highScoresDisplayEl.classList.add("hide");
+  playAgainBtnEl.classList.add("hide");
+  clearHighScoresBtnEl.classList.add("hide");
+  // show quiz-content element
+  quizScreenEl.classList.remove("hide");
+
+  displayQuestion(0);   
+
+  // set TimerIntervalId to setInterval function that decrements secondsLeft every second
+  timerIntervalId = setInterval(function () {
     
-function quizStart() {
-    document.getElementById('timer').value=m+":"+s+" remaining"
-    s=s-1
-    q=setTimeout("quizStart()", 1000)
-    if (s<1)
-    { m=m-1; s=59;}
+    secondsLeft--;
+    timeLeftEl.textContent = secondsLeft;
+    if (secondsLeft <= 0) {
+      stopGame();
+    }
+  }, 1000);
+}
+
+// create function to display a question and possible choices: 
+function displayQuestion(questionIndex) {
+  if (!questions[questionIndex]) {
+    stopGame();   
+  } else {
+  quizScreenEl.textContent = "";
+
+  //get questions[questionIndex]
+  var currentQuestion = questions[questionIndex];
+  //print questions to the page
+
+  var questionToWrite = document.createElement("h2");
+  questionToWrite.textContent = currentQuestion.question;
+
+  //use data attribute to know which index the question is
+  quizScreenEl.setAttribute("data-index", questionIndex);
+  quizScreenEl.appendChild(questionToWrite);
+  //loop through choices and print out choices to the page (make them buttons)
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+  
+    var answerButton = document.createElement("button");
+    answerButton.textContent = currentQuestion.choices[i];
+
+    answerButton.classList.add("btn");
+    answerButton.classList.add("btn-primary");
+    quizScreenEl.appendChild(answerButton);
+  }
+}
+}
+// create function to handle users answering:
+function userAnswerHandler(event) {
+  event.preventDefault();
+  //use event delegation to make sure button was clicked
+  var elementClicked = event.target;
+  if (elementClicked.matches("button") == true) {
+    //read data attribute of what questions we answered (index)
+   
+    var questionAnswered = elementClicked.parentElement.getAttribute("data-index");
     
-    if (m<0)
-    {
-    quizStop();
-    }
-    }
-    function quizStop()
-    {
-    clearTimeout(q)
-    document.getElementById('timer').value="Time's Up!"
+    //check to see if choice picked is same as questions correct answer
+    if (elementClicked.textContent == questions[questionAnswered].answer) {
+      score++;
+      //change info text to correct w/ class text-sucess
+      answerInfoTextEl.textContent = "Correct!";
+      answerInfoTextEl.classList.remove("text-danger");
+      answerInfoTextEl.classList.add("text-success");
+      //remove hide class from answer info
+      answerInfoEl.classList.remove("hide");
+      //on timeout add hide class to answer info
+      setTimeout(function(){
+        answerInfoTextEl.classList.remove("text-success");
+        answerInfoEl.classList.add("hide");
+      }, 1000);
+
+    } else { 
+      
+      secondsLeft = secondsLeft - 15;
+
+      //change info text to wrong w/ class text-danger
+      answerInfoTextEl.textContent = "Wrong!";
+      answerInfoTextEl.classList.remove("text-success");
+      answerInfoTextEl.classList.add("text-danger");
+      //remove hide class from answer info
+      answerInfoEl.classList.remove("hide");
+      //on timeout add hide class to answer info
+      setTimeout(function(){
+        answerInfoTextEl.classList.remove("text-danger");
+        answerInfoEl.classList.add("hide");
+      }, 1000);
     }
 
-    const Questions = [{
-        id: 0,
-        q: "Inside which HTML element do we put the JavaScript?",
-        a: [{ text: "<scripting>", isCorrect: false },
-            { text: "<javascript>", isCorrect: false },
-            { text: "<script>", isCorrect: true },
-            { text: "<js>", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 1,
-        q: "Where is the correct place to insert a JavaScript?",
-        a: [{ text: "The <head> section", isCorrect: false, isSelected: false },
-            { text: "The <body> section", isCorrect: false },
-            { text: "Both the <head> and <body> section", isCorrect: true },
-            { text: "Outside the <body> section", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 2,
-        q: "How to write an IF statement in JavaScript?",
-        a: [{ text: "if i = 5", isCorrect: false },
-            { text: "if (i == 5)", isCorrect: true },
-            { text: "if i == 5 then", isCorrect: false },
-            { text: "if i = 5 then", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 3,
-        q: "How can you add a comment in a JavaScript?",
-        a: [{ text: "<!-- This is a comment -->", isCorrect: false, isSelected: false },
-            { text: "//This is a comment", isCorrect: true },
-            { text: "'This is a comment", isCorrect: false },
-            { text: "*This is a comment*", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 4,
-        q: "How do you round the number 7.25, to the nearest integer?",
-        a: [{ text: "round(7.25)", isCorrect: false, isSelected: false },
-            { text: "rnd(7.25)", isCorrect: false },
-            { text: "Math.round(7.25)", isCorrect: true },
-            { text: "Math.rnd(7.25)", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 5,
-        q: "Which event occurs when the user clicks on an HTML element?",
-        a: [{ text: "onclick", isCorrect: true, isSelected: false },
-            { text: "onmouseclick", isCorrect: false },
-            { text: "onmouseover", isCorrect: false },
-            { text: "onchange", isCorrect: false }
-        ]
-  
-    },
-  
-]
-  
-// Set start
-var start = true;
-  
-// Iterate
-function iterate(id) {
-  
-    // Getting the result display section
-    var result = document.getElementsByClassName("result");
-    result[0].innerText = "";
-  
-    // Getting the question
-    const question = document.getElementById("question");
-  
-  
-    // Setting the question text
-    question.innerText = Questions[id].q;
-  
-    // Getting the options
-    const ch1 = document.getElementById('ch1');
-    const ch2 = document.getElementById('ch2');
-    const ch3 = document.getElementById('ch3');
-    const ch4 = document.getElementById('ch4');
-  
-  
-    // Providing option text 
-    ch1.innerText = Questions[id].a[0].text;
-    ch2.innerText = Questions[id].a[1].text;
-    ch3.innerText = Questions[id].a[2].text;
-    ch4.innerText = Questions[id].a[3].text;
-  
-    // Providing the true or false value to the options
-    ch1.value = Questions[id].a[0].isCorrect;
-    ch2.value = Questions[id].a[1].isCorrect;
-    ch3.value = Questions[id].a[2].isCorrect;
-    ch4.value = Questions[id].a[3].isCorrect;
-  
-    var selected = "";
-  
-    // Show selection for ch1
-    ch1.addEventListener("click", () => {
-        ch1.style.backgroundColor = "lightgoldenrodyellow";
-        ch2.style.backgroundColor = "lightskyblue";
-        ch3.style.backgroundColor = "lightskyblue";
-        ch4.style.backgroundColor = "lightskyblue";
-        selected = ch1.value;
-    })
-  
-    // Show selection for ch2
-    ch2.addEventListener("click", () => {
-        ch1.style.backgroundColor = "lightskyblue";
-        ch2.style.backgroundColor = "lightgoldenrodyellow";
-        ch3.style.backgroundColor = "lightskyblue";
-        ch4.style.backgroundColor = "lightskyblue";
-        selected = ch2.value;
-    })
-  
-    // Show selection for ch3
-    ch3.addEventListener("click", () => {
-        ch1.style.backgroundColor = "lightskyblue";
-        ch2.style.backgroundColor = "lightskyblue";
-        ch3.style.backgroundColor = "lightgoldenrodyellow";
-        ch4.style.backgroundColor = "lightskyblue";
-        selected = ch3.value;
-    })
-  
-    // Show selection for ch4
-    ch4.addEventListener("click", () => {
-        ch1.style.backgroundColor = "lightskyblue";
-        ch2.style.backgroundColor = "lightskyblue";
-        ch3.style.backgroundColor = "lightskyblue";
-        ch4.style.backgroundColor = "lightgoldenrodyellow";
-        selected = ch4.value;
-    })
-  
-    // Grabbing the evaluate button
-    const evaluate = document.getElementsByClassName("submit");
-  
-    // Evaluate method
-    evaluate[0].addEventListener("click", () => {
-        if (selected == "true") {
-            result[0].innerHTML = "Correct!";
-            result[0].style.color = "green";
-        } else {
-            result[0].innerHTML = "Oops, not quite!";
-            result[0].style.color = "red";
-        }
-    })
+    //get index of next question (this question's index + 1)
+    var nextQuestionIndex = parseInt(questionAnswered) +1;
+    
+      displayQuestion(nextQuestionIndex);
+   
+  }
 }
+
+// create a function to stop the game:
+function stopGame(event) {
   
-if (start) {
-    iterate("0");
+  clearInterval(timerIntervalId);
+  //clearInterval() to stop the time
+  //hide quiz-content element
+  quizScreenEl.classList.add("hide");
+  //show post-game-screen
+  playAgainBtnEl.classList.remove("hide");
+  postGameScreenEl.classList.remove("hide");
+  
+  //print out user score
+  score = score + secondsLeft;
+  userScoreEl.textContent = score;
+
 }
+
+function addHighscoreHandler(event){
+  event.preventDefault();
+  console.log("adding to highscores");
   
-// Next button and method
-const next = document.getElementsByClassName('next')[0];
-var id = 0;
+  var initialsToAdd = userInitials.value.trim();
+
+  if (!initialsToAdd){
+    return false;
+  }
+  //trims input to first 3 
+  initialsToAdd = initialsToAdd.slice(0, 3);
+
+  var hsObjectToAdd = {
+    hsInitials: initialsToAdd.toUpperCase(),
+    hsScore: parseInt(score)
+  }
   
-next.addEventListener("click", () => {
-    start = false;
-    if (id < 5) {
-        id++;
-        iterate(id);
-        console.log(id);
+  highScoresList.push(hsObjectToAdd);
+
+  showHighscores();
+
+}
+
+function showHighscores(){
+  clearInterval(timerIntervalId);
+  postGameScreenEl.classList.add("hide");
+  startScreenEl.classList.add("hide");
+  quizScreenEl.classList.add("hide");
+  playAgainBtnEl.classList.remove("hide");
+  clearHighScoresBtnEl.classList.remove("hide");
+  //sort highscores by score
+  highScoresList.sort(function (a, b) {
+    return a.hsScore - b.hsScore
+  })
+  console.log(highScoresList);
+
+  highScoresListEl.innerHTML = "";
+
+  for (var i = 0; i < highScoresList.length; i ++){
+    var highScore = highScoresList[i];
+
+    var li = document.createElement("div");
+    li.textContent = (i + 1) + ".     " + highScore.hsInitials + " - " + highScore.hsScore ;
+
+    //i want to alternate bg of each li so i'll check if i is even
+    if( i % 2 === 0){
+      li.classList.add("list-group-item-light", "high-score-even");
+    } else {
+      li.classList.add("list-group-item-secondary", "high-score-odd");
     }
-  
-})
+
+
+    highScoresListEl.appendChild(li);
+  }
+
+  highScoresDisplayEl.classList.remove("hide");
+
+  localStorage.setItem("highScoresList", JSON.stringify(highScoresList));
+
+}
+
+function clearHighscoreHandler(){
+  highScoresList = [];
+  showHighscores();
+
+}
+
+//start game button
+startGameBtnEl.addEventListener("click", startGameHandler);
+//quiz content (for answring a question) --> use event delegation
+quizScreenEl.addEventListener("click", userAnswerHandler);
+
+//view highscores link
+viewHighscoresAEl.addEventListener("click", showHighscores);
+
+  //play again button
+playAgainBtnEl.addEventListener("click", startGameHandler);
+//add high score button
+gameEndFormEl.addEventListener("submit", addHighscoreHandler);
+//clear high scores
+
+clearHighScoresBtnEl.addEventListener("click", clearHighscoreHandler);
